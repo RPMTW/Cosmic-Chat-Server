b5e89161c1cced238f345b8e9c121da69a0346b6
菘 菘 <>
2021-07-22 08:17:12 +0000

    回覆訊息與WebHook實裝

--- discord/SendMessage.js
+++ discord/SendMessage.js
@@ -1,5 +1,15 @@
-module.exports = async(ChannelID, client, Msg) => {
-  let channel = client.channels.cache.find(channel => channel.id === ChannelID);
-  if (!channel) return;
-  channel.send(Msg);
+module.exports = async (ChannelID, client, Msg, UUID, UserName) => {
+  let ischannel = client.channels.cache.find(channel => channel.id === ChannelID);
+  if (!ischannel) return;
+  try {
+    const channel = client.channels.cache.get(ChannelID);
+    const webhooks = await channel.fetchWebhooks();
+    const webhook = webhooks.first();
+    webhook.send(Msg, {
+      username: UserName,
+      avatarURL: `https://crafatar.com/avatars/${UUID}`
+    });
+  } catch (error) {
+    console.error('Error trying to send: ', error);
+  }
 };
--- index.js
+++ index.js
@@ -13,40 +13,65 @@ const client = new Discord.Client()
 const log = new Discord.WebhookClient("832853964819136532", process.env['WebHookToken']);
 
 let onlineCount = 0;
+let isready = false;
 
 require("./discord/init")(client, log)
 
 client.on("message", async (msg) => {
   if (msg.author.bot) return;
   if (msg.channel.id === "831494456913428501") {
-    let data = { "Type": "Client", "Message": msg.content, "UserName": msg.author.tag, "IP": "" }
-    log.send(data);
-    io.emit("broadcast", data);
+    let data;
+    if (msg.reference) { //如果該訊息是回覆的訊息
+      msg.channel.messages.fetch(msg.reference.messageID).then(message => {
+        let tag = message.author.tag;
+        if (tag === msg.author.tag) {
+          tag = "自己"
+        }
+        data = { "Type": "Client", "Message": `§a回覆 §6${tag} §b${message.content} §a-> §f${msg.content}`, "UserName": msg.author.tag, "IP": "" }
+        log.send(data);
+        io.emit("broadcast", data);
+      })
+        .catch(console.error);
+
+    } else {
+      data = { "Type": "Client", "Message": msg.content, "UserName": msg.author.tag, "IP": "" }
+      log.send(data);
+      io.emit("broadcast", data);
+    }
   }
 });
+
+client.on('ready', () => {
+  isready = true;
+})
 
 io.on('connection', function(socket) {
   onlineCount++;
   log.send(onlineCount);
-  client.user.setActivity(`宇宙通訊共有 ${onlineCount} 個人`, { type: 'WATCHING' })
-    .catch(console.error);
+  if (isready) {
+    client.user.setActivity(`宇宙通訊共有 ${onlineCount} 個玩家`, { type: 'WATCHING' })
+      .catch(console.error);
+  }
   try {
     socket.on('message', function(data) {
       console.log('new data: ' + data);
       let JsonData = JSON.parse(data);
       let Message = JsonData.Message;
       let UserName = JsonData.UserName;
+      let UUID = JsonData.UUID;
       let IP = JsonData.IP;
+
+      if (Message == null) return;
 
       log.send(data);
 
       if (BanIp.includes(IP)) {
-        data = `{\"Type\":\"Server\",\"Message\":\"Ban\",\"UserName\":\"${UserName}\",\"UUID\":\"${JsonData.UUID}\",\"IP\":\"${IP}\"}`
-        io.emit("broadcast", data);
+        data = `{\"Type\":\"Server\",\"Message\":\"Ban\",\"UserName\":\"${UserName}\",\"UUID\":\"${UUID}\",\"IP\":\"${IP}\"}`
+        io.emit("broadcas", data);
         return;
       };
-      
-      require("./discord/SendMessage")("831494456913428501", client, `**<${UserName}>** ${Message}`)
+
+      require("./discord/SendMessage")("831494456913428501", client, Message, UUID, UserName)
       io.emit("broadcast", data);
     });
     socket.on('disconnect', () => {
